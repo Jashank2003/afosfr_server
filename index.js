1683a3eaf437fbd21ee4ab805cccfd1e2fff9ad6
Jashank <jashankjain224@gmail.com>
2024-05-15 00:07:40 +0530

    newchange

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const io = require("socket.io")(process.env.PORT || 5000,{
   cors:{
-    origin:"*",
+    origin:"http://localhost:3000",
     methods:["GET","POST"],
   },
 });
 
 // console.log("heelo")
-const data = "hehe";
+const data = {
+  orderId: "123456",
+  name: "John Doe",
+  order: "1 * Corn Pizza, 2 * Mint Mojito",
+  amount: 15.99
+};
+
 io.on('connection', (socket) => {
   console.log('A user connected');
-  socket.emit("newOrder",data);
-  // Handle socket events here
-  socket.on("newOrder" , (data)=>{
-    console.log(data);
 
-  })
+  // Handle socket events here
+  socket.on("newOrder", (data) => {
+    console.log("New order received from Flutter client:", data);
+    io.emit("fetchOrder", data); // Emit to all connected clients, including Next.js
+  });
+
+
+  
   socket.on("disconnect",()=>{
     console.log("user disconnected");
   })
