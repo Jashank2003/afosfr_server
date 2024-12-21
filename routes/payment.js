5a124857056257000360e25161a529523c718773
Jashank <jashankjain224@gmail.com>
2024-12-22 04:26:33 +0530

    payments after resolving env locha

---
+++ routes/payment.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { createOrder } from '../functions/orders.js';
+
+const paymentRouter = express.Router();
+
+paymentRouter.post('/api/order', async (req, res) => {
+    try {
+        const orderData = req.body;
+        const result = await createOrder(orderData); // Execute the logic from orders.js
+        console.log("yeh order succesfully created");
+        console.log(result);
+        res.status(201).json(result);
+    } catch (error) {
+        res.status(500).json({
+            message: 'An error occurred while processing the order',
+            error: error.message,
+        });
+    }
+});
+
+export default paymentRouter;
